87f7cb1118540c598ef540812b409e839f824046
elnurbx-work <elnurbx@gmail.com>
2026-04-08 11:11:46 +0400

    add menu

--- test.c
+++ test.c
@@ -1,19 +1,61 @@
 #include <stdio.h>
 
-int main(void) {
-    int choice;
+#define NAME_SIZE 50
+#define SURNAME_SIZE 50
+#define EMAIL_SIZE 100
 
-    printf("Student Manager\n");
+typedef struct {
+    int id;
+    char name[NAME_SIZE];
+    char surname[SURNAME_SIZE];
+    int age;
+    char email[EMAIL_SIZE];
+} Student;
+
+void showMenu(void);
+void handleChoice(int choice);
+
+void showMenu(void) {
+    printf("\nStudent Manager\n");
     printf("1 -- Istifadeci elave et\n");
     printf("2 -- Istifadecini sil\n");
     printf("3 -- Istifadecileri goster\n");
     printf("4 -- Secilmis istifadecini yenile\n");
     printf("5 -- Proqramdan cix\n");
+}
 
-    printf("Seciminizi daxil edin: ");
-    scanf("%d", &choice);
+void handleChoice(int choice) {
+    switch (choice) {
+        case 1:
+            printf("Add hissesi sonra yazilacaq.\n");
+            break;
+        case 2:
+            printf("Delete hissesi sonra yazilacaq.\n");
+            break;
+        case 3:
+            printf("Show hissesi sonra yazilacaq.\n");
+            break;
+        case 4:
+            printf("Update hissesi sonra yazilacaq.\n");
+            break;
+        case 5:
+            printf("Programdan cixilir...\n");
+            break;
+        default:
+            printf("Yanlis secim etdiniz.\n");
+    }
+}
 
-    printf("Siz %d secdiniz.\n", choice);
+int main(void) {
+    int choice = 0;
+
+    while (choice != 5) {
+        showMenu();
+        printf("Seciminizi daxil edin: ");
+        scanf("%d", &choice);
+
+        handleChoice(choice);
+    }
 
     return 0;
 }
